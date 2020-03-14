1f2e878ed35ffe3090075b223eb289bdbb067b11
Sergio Ramirez <47802275+chechoXR@users.noreply.github.com>
2020-03-13 19:58:35 -0500

    4to

--- services.js
+++ services.js
@@ -47,3 +47,41 @@ appServer.get('/myBasicInfo',
     res.send('HELLO WORLD WITH EXPRESS!!! my name is Jaime Leon');
 }   
 );
+
+
+
+var pandemias = {}
+
+appServer.get ('/info-pandemia:id',
+(req, res) => {
+
+    var ID= parseInt(id);
+    var nombre;
+    var sintomas;
+    var recomendaciones;
+    var cantidadPaises;
+    var cantidadInfectados=0;
+    var cantidadRecuperados=0;
+    var cantidadMuertes=0;
+    var paises;
+
+    
+    nombre = pandemias[ID].nombre;
+    sintomas = pandemias[ID].sintomas;
+    recomendaciones = pandemias[ID].recomendaciones;
+    cantidadPaises = pandemias[ID].paises.length;
+    
+    for(var i = 0; i< cantidadPaises; i++){
+        cantidadInfectados  +=    pandemias[ID].paises[i].infectados;
+        cantidadRecuperados +=    pandemias[ID].paises[i].recuperados;
+        cantidadMuertes     +=    pandemias[ID].paises[i].muertos;
+    }
+    
+    paises = pandemias[ID].paises;
+
+
+    var info = {ID, nombre, sintomas, recomendaciones, cantidadPaises, cantidadInfectados, cantidadRecuperados, cantidadMuertes, paises};
+
+        res.send (info);
+}
+);
